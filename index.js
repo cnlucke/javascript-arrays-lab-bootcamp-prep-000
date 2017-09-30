507d05df6719ecba5a8c71f979de80467c1a5168
cnlucke <cristy.lucke@gmail.com>
2017-09-30 15:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,21 +18,33 @@ function destructivelyRemoveLastKitten() {
   kittens.pop
   return kittens
 }
-  3) Arrays destructivelyRemoveLastKitten() removes the last kitten from the kittens array:
-     ReferenceError: destructivelyRemoveLastKitten is not defined
-      at Context.<anonymous> (test/index-test.js:29:7)
-4) Arrays destructivelyRemoveFirstKitten() removes the First kitten from the kittens array:
-   ReferenceError: destructivelyRemoveFirstKitten is not defined
-    at Context.<anonymous> (test/index-test.js:37:7)
-5) Arrays appendKitten(name) appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
-   ReferenceError: appendKitten is not defined
-    at Context.<anonymous> (test/index-test.js:45:14)
-6) Arrays prependKitten(name) prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
-   ReferenceError: prependKitten is not defined
-    at Context.<anonymous> (test/index-test.js:53:14)
-7) Arrays removeLastKitten() removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged:
-   ReferenceError: removeLastKitten is not defined
-    at Context.<anonymous> (test/index-test.js:61:14)
-8) Arrays removeFirstKitten() removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
-   ReferenceError: removeFirstKitten is not defined
-    at Context.<anonymous> (test/index-test.js:69:14)
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift
+  return kittens
+}
+
+function appendKitten(name) {
+  newArray = kittens
+  newArray.push(name)
+  return newArray
+}
+
+function prependKitten(name) {
+  newArray = kittens
+  newArray.unshift(name)
+  return newArray
+}
+
+function removeLastKitten() {
+  newArray = kittens
+  newArray.pop
+  return newArray
+}
+
+function removeFirstKitten() {
+  newArray = kittens
+  newArray.shift
+  return newArray
+}
+
