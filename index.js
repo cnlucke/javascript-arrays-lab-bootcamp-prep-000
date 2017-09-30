fe39ccdc166e1d6a1536882687daea84466b4e22
cnlucke <cristy.lucke@gmail.com>
2017-09-30 15:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 //const kittens = "I don't do much"
 
-function kittens() {
+function setKittens() {
   kittens = ["Milo", "Otis", "Garfield"]
 }
 
 function destructivelyAppendKitten(name) {
-  kittens()
+  setKittens()
   kittens.push(name)
   return kittens
 }
 
 function destructivelyPrependKitten(name) {
-  kittens()
+  setKittens()
   kittens.unshift(name)
   return kittens
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens()
+  setKittens()
   kittens.pop()
   return kittens
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens()
+  setKittens()
   kittens.shift()
   return kittens
 }
 
 function appendKitten(name) {
-  kittens()
+  setKittens()
   var newArray = kittens
   newArray.push(name)
   return newArray
 }
 
 function prependKitten(name) {
-  kittens()
+  setKittens()
   var newArray = kittens
   newArray.unshift(name)
   return newArray
 }
 
 function removeLastKitten() {
-  kittens()
+  setKittens()
   var newArray = kittens
   newArray.pop()
   return newArray
 }
 
 function removeFirstKitten() {
-  kittens()
+  setKittens()
   var newArray = kittens
   newArray.shift()
   return newArray
