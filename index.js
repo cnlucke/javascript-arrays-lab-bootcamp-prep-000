d8f8fddc917f0f9597450f9fe1229f8b66f24524
cnlucke <cristy.lucke@gmail.com>
2017-09-30 15:51:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,44 +5,52 @@ function kittens() {
 }
 
 function destructivelyAppendKitten(name) {
+  kittens()
   kittens.push(name)
   return kittens
 }
 
 function destructivelyPrependKitten(name) {
+  kittens()
   kittens.unshift(name)
   return kittens
 }
 
 function destructivelyRemoveLastKitten() {
+  kittens()
   kittens.pop()
   return kittens
 }
 
 function destructivelyRemoveFirstKitten() {
+  kittens()
   kittens.shift()
   return kittens
 }
 
 function appendKitten(name) {
+  kittens()
   var newArray = kittens
   newArray.push(name)
   return newArray
 }
 
 function prependKitten(name) {
+  kittens()
   var newArray = kittens
   newArray.unshift(name)
   return newArray
 }
 
 function removeLastKitten() {
+  kittens()
   var newArray = kittens
   newArray.pop()
   return newArray
 }
 
 function removeFirstKitten() {
+  kittens()
   var newArray = kittens
   newArray.shift()
   return newArray
